bc2b0ebaae8cd0a9112178b561947c76840e9b21
Daco <dacokim32@gmail.com>
2021-11-17 22:06:42 +0900

    mtmfield commit

--- movies(정션테이블)/models.py
+++ movies(정션테이블)/models.py
@@ -9,7 +9,6 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     date_of_birth = models.DateField()
-    movie = models.ManyToManyField("Movie")
 
     class Meta:
         db_table = 'actors'
@@ -24,12 +23,12 @@ class Movie(models.Model):
         db_table = 'movies'
 
 
-# class Actors_movies(models.Model):
-#     actor = models.ForeignKey('Actor', on_delete=models.CASCADE)
-#     movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
+class Actors_movies(models.Model):
+    actor = models.ForeignKey('Actor', on_delete=models.CASCADE)
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
 
-#     class Meta:
-#         db_table = 'actors_movies'
+    class Meta:
+        db_table = 'actors_movies'
 
 
 
--- movies(정션테이블)/views.py
+++ movies(정션테이블)/views.py
@@ -10,41 +10,43 @@ from .models import *
 
 
 class ActorsView(View):
-    
     def get(self, request):
-        actor_data = Actor.objects.all()
+        actors_data = Actor.objects.all()
         result_list = []
-        for i in actor_data:
-            middle = i.movie.all()
+
+        for i in actors_data:
+            b = Actors_movies.objects.filter(actor=i)
             result_list.append({
-                "성" : i.last_name,
                 "이름" : i.first_name,
-                "출연영화" : [j.title for j in middle]
+                "성" : i.last_name,
+                "출연영화" : [j.movie.title for j in b],
+                
                 })
-            
+
+            # 콘솔 확인용 for문, 1번은 정션테이블의 id, 2번은 movie 컬럼의 id이기 때문에 2번을 사용해야함!
+            # for k in b:
+            #     print(f"1번 j          = {k.movie}")
+            #     print(f"2번 j.movie    = {k.movie}")
+            #     print(f"3번 j.movie.id = {k.movie.id}")
+
         return JsonResponse({'result':result_list}, status=200)
 
 
 
 class MoviesView(View):
     
-    # 오브젝트 가져오면 정션테이블 붙이고 올로 가져올 수 있음
-
     def get(self, request):
-        
         
         movies_data = Movie.objects.all()
         result_list = []
         for i in movies_data:
-            b = i.actor_set.all()
-            print(f" b = {b}")
+            b = Actors_movies.objects.filter(movie=i)
             actor_list = []
             for j in b:
                 actor_list.append({
-                "성" : j.last_name,
-                "이름" : j.first_name
+                "성" : j.actor.last_name,
+                "이름" : j.actor.first_name
                 })
-                print(f" actor_list = {actor_list}")
             result_list.append({
                 "타이틀" : i.title,
                 "러닝타임" : i.running_time,
